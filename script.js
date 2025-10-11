1e253a63fa77c9f8d1e46fd2b8c419fb1a7cea2b
flickenhenrey <flickenhenrey@gmail.com>
2025-10-10 17:35:44 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,43 @@
+// ADD YOUR ANIME HERE
+const animeList = [
+    {
+        title: 'DOG & CHAINSAW',
+        description: 'Your anime description here',
+        image: 'images/dogchainsaw.png',
+        url: 'https://flickenhenrey.github.io/coolanime/',
+        episodes: 12
+    },
+    // Add more anime:
+    // {
+    //     title: 'Another Anime',
+    //     description: 'Description here',
+    //     image: 'images/anotheranime.png',
+    //     url: 'https://flickenhenrey.github.io/anotheranime/',
+    //     episodes: 24
+    // },
+];
+
+const animeListEl = document.getElementById('anime-list');
+
+function loadAnime() {
+    animeListEl.innerHTML = '';
+
+    animeList.forEach(anime => {
+        const card = document.createElement('a');
+        card.className = 'anime-card';
+        card.href = anime.url;
+        card.innerHTML = `
+            <img src="${anime.image}" alt="${anime.title}" class="anime-image">
+            <div class="anime-info">
+                <div class="anime-title">${anime.title}</div>
+                <div class="anime-description">${anime.description}</div>
+                <div class="anime-meta">
+                    <span>📺 ${anime.episodes} Episode${anime.episodes !== 1 ? 's' : ''}</span>
+                </div>
+            </div>
+        `;
+        animeListEl.appendChild(card);
+    });
+}
+
+loadAnime();
